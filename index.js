e7cbf7b15c61a4302ec888e79d8e0e6735788071
Renegade Confidence <confidencenyirenda@Renegades-MacBook-Air.local>
2025-10-21 15:13:25 +0200

    change js to use async

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=blockchain")
-    .then(res => res.json())
-    .then(data => {
+try {
+    const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=blockchain")
+    const data = await res.json()
+    if (!res.ok) {
+        throw Error("Something went wrong")
+    }
         document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
-    })
-    .catch(err => {
-        // Use a default background image/author
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-)`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
-    })
+        document.getElementById("author").textContent = `By: ${data.user.name}`
+        
+} catch(err) {
+    //Use a default background image/author
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080)`
+    document.getElementById("author").textContent = `By: Dodi Achmad`
+}
 
-fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
-    .then(res => {
-        if (!res.ok) {
-            throw Error("Something went wrong")
-        }
-        return res.json()
-    })
-    .then(data => {
-        document.getElementById("crypto-top").innerHTML = `
+try {
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+    const data = await res.json()
+    if (!res.ok) {
+        throw Error("Something went wrong")
+    }
+    document.getElementById("crypto-top").innerHTML = `
             <img src=${data.image.small} />
             <span>${data.name}</span>
         `
@@ -28,8 +28,9 @@ fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
             <p>👆: $${data.market_data.high_24h.usd}</p>
             <p>👇: $${data.market_data.low_24h.usd}</p>
         `
-    })
-    .catch(err => console.error(err))
+} catch(err) {
+    console.error(err)
+}
 
 function getCurrentTime() {
     const date = new Date()
@@ -38,21 +39,20 @@ function getCurrentTime() {
 
 setInterval(getCurrentTime, 1000)
 
-navigator.geolocation.getCurrentPosition(position => {
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
-        .then(res => {
-            if (!res.ok) {
-                throw Error("Weather data not available")
-            }
-            return res.json()
-        })
-        .then(data => {
-            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+navigator.geolocation.getCurrentPosition(async position => {
+    try {
+        const res = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+        const data = await res.json()
+        if (!res.ok) {
+            throw Error("Weather data not available")
+        }
+        const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
             document.getElementById("weather").innerHTML = `
                 <img src=${iconUrl} />
                 <p class="weather-temp">${Math.round(data.main.temp)}º</p>
                 <p class="weather-city">${data.name}</p>
             `
-        })
-        .catch(err => console.error(err))
+    } catch(err) {
+        console.error(err)
+    }
 });
